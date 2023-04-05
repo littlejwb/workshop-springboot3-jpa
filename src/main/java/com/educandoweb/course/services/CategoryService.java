1efa0e48ea8dfa8aff8d956dd099f833e0475792
Juliana Ricardo <szric.ju@gmail.com>
2023-04-05 14:27:09 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	//injeção de dependência por construtor
+	private CategoryRepository repository;
+
+	public CategoryService(CategoryRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
